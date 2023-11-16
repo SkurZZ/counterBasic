695b12b00a65c12d65e9f0d800ed691ab11a9c3c
Fábio <fabiiohenrique.or@gmail.com>
2023-11-15 22:07:17 -0300

    Added new add and remove options

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btnDecrease = document.getElementById('decrease')
 const btnReset = document.getElementById('reset')
 const btnIncrease = document.getElementById('increase')
+const moreoneHundred = document.getElementById('moreoneHundred')
+const minusHundred = document.getElementById('minusHundred')
+const thousandMore = document.getElementById('thousandMore')
+const minusThousand = document.getElementById('minusThousand')
+
 let countNumber = document.getElementById('number')
 let count = 0
 
@@ -19,6 +24,25 @@ btnReset.addEventListener("click", function () {
     colorNumbers()
 })
 
+minusHundred.addEventListener("click", function () {
+    countNumber.innerText = count -= 100
+    colorNumbers()
+})
+
+moreoneHundred.addEventListener("click", function () {
+    countNumber.innerText = count += 100
+    colorNumbers()
+})
+
+minusThousand.addEventListener("click", function () {
+    countNumber.innerText = count -= 1000
+    colorNumbers()
+})
+
+thousandMore.addEventListener("click", function () {
+    countNumber.innerText = count += 1000
+    colorNumbers()
+})
 function colorNumbers() {
     if (count < 0) {
         countNumber.style.color = "red";
