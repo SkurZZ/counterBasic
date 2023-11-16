6d514e3bdbe44437f28da40ea86a9a37b053c43b
Fábio <fabiiohenrique.or@gmail.com>
2023-11-15 22:01:35 -0300

    Finalized application javascript

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-btnDecrease = document.getElementById('decrease')
-btnReset = document.getElementById('reset')
-btnIncrease = document.getElementById('increase')
-countNumber = document.getElementById('number')
+const btnDecrease = document.getElementById('decrease')
+const btnReset = document.getElementById('reset')
+const btnIncrease = document.getElementById('increase')
+let countNumber = document.getElementById('number')
+let count = 0
+
+btnDecrease.addEventListener("click", function () {
+    countNumber.innerText = count -= 1
+    colorNumbers()
+})
+
+btnIncrease.addEventListener("click", function () {
+    countNumber.innerText = count += 1
+    colorNumbers()
+})
+
+btnReset.addEventListener("click", function () {
+    countNumber.innerText = count = 0
+    colorNumbers()
+})
+
+function colorNumbers() {
+    if (count < 0) {
+        countNumber.style.color = "red";
+    } else if (count === 0) {
+        countNumber.style.color = "black";
+    } else {
+        countNumber.style.color = "green";
+    }
+}
