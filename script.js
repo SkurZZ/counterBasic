b54dfe53fb61e1074f52197d372f77b2ce03a6c6
Fábio <fabiiohenrique.or@gmail.com>
2023-11-15 21:41:02 -0300

    Finalized HTML and CSS

---
+++ script.js
@@ -0,0 +1,4 @@
+btnDecrease = document.getElementById('decrease')
+btnReset = document.getElementById('reset')
+btnIncrease = document.getElementById('increase')
+countNumber = document.getElementById('number')
